bd33a6dd35b10f9e4c858418f3397411faa65876
Lyub1m1y <dokhoyan-leva@mail.ru>
2022-10-18 23:06:33 +0400

    add 5.7.cpp [a)]

--- task8/5.7.cpp
+++ task8/5.7.cpp
@@ -1,4 +1,5 @@
 /*"Copyright [2022] <katrinap>"*/
+#include <cctype>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -6,61 +7,90 @@ using namespace std;
 
 class Element {
  public:
-  string formula, name;
-  double mass;
-  int num;
+  string name;
+  int num = 0;  //количество
+  int cost = 0;
 };
 
-int strLength(string temp) {
-  int count = 0;
-  while (temp[count] != '\0') {
-    count++;
+int fillingElements() {  // 5.7 a)
+  int n = 0;
+  cout << "How many items do you want to add: ";
+  cin >> n;
+  Element element[n];
+  for (int i = 0; i < n; i++) {
+    cout << "------------------------------------" << endl
+         << "Enter information about the item:" << endl
+         << "1) name" << endl
+         << "2) num" << endl
+         << "3) cost" << endl
+         << "------------------------------------" << endl;
+    cin >> element[i].name;
+    cin >> element[i].num;
+    cin >> element[i].cost;
+    ofstream out("text7.txt", ios::app);
+    out << element[i].name << '\t' << element[i].num << '\t' << element[i].cost
+        << endl;
+    out.close();
   }
-  return count;
-}
-
-int checkRow(string numConsol, string temp) {
-  int tempLength = strLength(temp);
-  int numConsolLength = strLength(numConsol);
-  int counterTrue = 0;
-  int flag = 0;
-  for (int i = 0; i < tempLength; i++) {
-    if (temp[i] == '\t') {
-      for (int j = 0, i2 = i; j < numConsolLength; j++, i2++) {
-        if (numConsol[j] == temp[i2 + 1]) {
-          counterTrue++;
-        } else {
-          counterTrue = 0;
-        }
-      }
-    }
-  }
-  if (counterTrue == numConsolLength) {
-    flag = 1;
-  }
-  return flag;
+  return 0;
 }
 
 int main() {
-  cout << "Enter the number of the item you want to find: ";
-  string numConsol;
-  cin >> numConsol;
-  fstream in("text5.txt");
-  string str, temp;
-  int flagWhile = 0, flagCout = 0;
-  while (getline(in, temp) && (flagWhile == 0)) {
-    str = temp;
-    if (checkRow(numConsol, temp) == 1) {
-      cout << temp << endl;
-      flagCout = 0;
-      flagWhile = 1;
-    } else {
-      flagCout = 1;
-    }
+  int n = 0;
+  cout << "Choose which subroutine from 1 - 5 to run?" << endl;
+  cin >> n;
+  if (n == 1) {
+    fillingElements();
+    // } else if (n == 2) {
+    //   fillingElements();
+    // } else if (n == 3) {
+    //   fillingElements();
+    // } else if (n == 4) {
+    //   fillingElements();
+    // } else if (n == 5) {
+    //   fillingElements();
+  } else {
+    cout << "God, man, you can't choose exactly from 1 to 5 :(" << endl;
   }
-  if (flagCout == 1) {
-    cout << "Nothing found" << endl;
-  }
-  in.close();
   return 0;
 }
+
+// int strLength(string temp) {
+//   int count = 0;
+//   while (temp[count] != '\0') {
+//     count++;
+//   }
+//   return count;
+// }
+
+// int checkRow(string numConsol, string temp, int* sum) {
+//   int tempLength = strLength(temp);
+//   int numConsolLength = strLength(numConsol);
+//   int counterTrue = 0;
+//   int flag = 0;
+//   int flagForSum = 0;
+//   int flagForExitJ0 = 0;
+//   string tempStringInt[10];
+//   for (int i = 0, j = 0; i < tempLength; i++, j++) {
+//     if (temp[i] == '\t') {
+//       if (flagForSum != 2) {
+//         flagForSum++;
+//       } else if (flagForExitJ0 == 0) {
+//         j = 0;
+//         flagForExitJ0++;
+//       }
+//     }
+//     if (flagForSum == 2) {
+//       if (temp[i] != '\0') {
+//         tempStringInt[j] = temp[i];
+//       } else {
+//         flag = 1;
+//       }
+//     }
+//     if (flag == 1) {
+//       *sum = atoi(&tempStringInt);
+//       flag = 0;
+//     }
+//   }
+//   return flag;
+// }
