b458c1ca44f8ea681e44c7d0c00c408bc6736eac
Lyub1m1y <dokhoyan-leva@mail.ru>
2022-12-12 11:14:58 +0300

    add Class Work 09.12 English

---
+++ OP/C#/PR_30/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Привет, пидорасы");
+        }
+    }
+}
